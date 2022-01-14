cf896b89439337bc1a61b573fcb5e808f79eda2f
sayasurvey <sayasurvey@yahoo.co.jp>
2022-01-14 12:12:18 +0900

    Fix: seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,120 @@ Board.create!(
       title: "ピアノに叩かれながらも寝続ける猫",
       content: "鍵盤の動きに合わせて猫の体も上下に振動するも退く気配のない猫 猫の可愛さと飼い主の美しい音色も一緒に楽しめる動画",
       movie_url: "https://www.youtube.com/watch?v=4t0I10eMoJU"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=S2Vem8wk4fY"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=6QD3NN01sYo"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=MtK1vNca5mU"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=XmsU7tLyIac"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=djp_hurOBdE&t=55s"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=iFkxCHHhIS4"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=Au9XlHCdefs"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=smvcWcuUBFE"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=jBFZIc11wKU"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=pPiu-5CNyyI"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=0jw_okUV9S0"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=LStfxrTqLV0"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=hs4SRd51Wu0"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=ORrvGmCcnJc"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=XBBCPBxSzvI"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=L2HxNfHGdgY"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=iv8XSTNxkNY"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=9HU0hvakAmo"
+    },
+
+    {
+      title: "test",
+      content: "test",
+      movie_url: "https://www.youtube.com/watch?v=2--lmNFbI1M"
     }
   ]
 )
