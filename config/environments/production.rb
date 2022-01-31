2594287d23a4c4d3b65ac24b0289f3ff7676b676
sayasurvey <sayasurvey@yahoo.co.jp>
2022-02-01 01:52:18 +0900

    FIX:SendGrid Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
       port: '587',
       address: 'smtp.sendgrid.net',
       domain: 'herokuapp.com',
-      user_name: 'apikey',,
+      user_name: 'apikey',
       password: ENV['SENDGRID_API_KEY'],
       enable_starttls_auto: :plain,
       authentication: :true
