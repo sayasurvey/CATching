b2f05c7894b401575bc1d0bfcf84f2b7d132df37
sayasurvey <sayasurvey@yahoo.co.jp>
2022-02-01 20:30:37 +0900

    FIX: 本番のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,12 +94,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'cat-ching.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      enable_starttls_auto: true,
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['SMTP_USERNAME'],
-      password:             ENV['SMTP_PASSWORD'],
-      authentication:       :plain
+    address:              'smtp.gmail.com',
+    enable_starttls_auto: true,
+    port:                 587,
+    domain:               'smtp.gmail.com',
+    user_name:            'saya3133@gmail.com',
+    password:             'wrsogflgdcpkcwqw',
+    authentication:       :plain
     }
 end
