e585ccd0cf94a6197ef584fae4d42d78d8004e05
sayasurvey <sayasurvey@yahoo.co.jp>
2022-02-01 23:23:10 +0900

    FIX: OGPの修正 ADD: iconの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,23 +4,27 @@ module ApplicationHelper
       site: 'CATching',
       title: '押しネコが見つかる動画検索サイト',
       reverse: true,
-      separator: '|',
+      charset: 'utf-8',
       description: '押しネコが見つかる動画検索サイト',
       keywords: '猫, 動画, 検索',
       canonical: request.original_url,
-      noindex: ! Rails.env.production?,
+      separator: '|',
+      icon: [
+        { href: image_url('icon.png') },
+        { href: image_url('icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
       og: {
-        site_name: 'サイト名',
-        title: 'タイトル',
-        description: 'ディスクリプション',
+        site_name: :site,
+        title: :title,
+        description: :description,
         type: 'website',
         url: request.original_url,
         image: image_url('ogp.png'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@Runteq_sayaka',
+        site: '@Runteq_sayaka'
       }
     }
   end
