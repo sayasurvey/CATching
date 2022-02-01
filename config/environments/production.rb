0bc7e7f8109a5abd9bd64a4a39beb9be16e7d2aa
sayasurvey <sayasurvey@yahoo.co.jp>
2022-02-01 15:52:55 +0900

    FIX: SendGrid Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+    #メールの設定情報
     config.action_mailer.smtp_settings = {
       port: '587',
       address: 'smtp.sendgrid.net',
