563cc09ea9ba4e297a1b591af62c1e173bf36900
sayasurvey <sayasurvey@yahoo.co.jp>
2022-02-09 13:23:43 +0900

    FIX:コメントの削除 FIX:Twitterのハッシュタグ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   get 'boards/registration', to: 'boards#registration'
-  get 'boards/search', to: 'boards#search'
 
   resources :users, only: %i[new create]
   resources :boards
   resources :contacts, only: %i[new create]
   root to: 'boards#index'
   
-  get 'login', to: 'user_sessions#new'  # 追記
-  post 'login', to: 'user_sessions#create'  # 追記
-  delete 'logout', to: 'user_sessions#destroy'  # 追記
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
